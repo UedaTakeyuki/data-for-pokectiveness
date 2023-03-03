e4922175eb075a25a4e7244a99f1e90f8747ec3e
Takeyuki UEDA <ueda@latelierdueda.com>
2023-03-03 23:42:01 +0900

    first commit

---
+++ src/build-entry.js
@@ -0,0 +1,12 @@
+import pokedex from './pokedex';
+const Pokedex = pokedex
+
+// Named export
+export {
+  pokedex
+}
+
+// Default export
+export default {
+  pokedex
+};
---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+// https://qiita.com/shin_moto/items/d75f1694bccf2bb4302d
+module.exports = {
+  mode: 'development', // 開発モードか本番モードかを設定出来る
+  entry: './src/build-entry.js', // バンドルの起点となるファイル
+  output: {
+    library: 'Pokedex',
+    libraryTarget: 'umd',
+    filename: 'Pokedex.js',
+    globalObject: 'this',
+  }
+}
