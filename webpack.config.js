a74cfeba7e660e18f6658def38ee4a0f6fd12bd1
Takeyuki UEDA <ueda@latelierdueda.com>
2023-03-04 08:00:34 +0900

    zip-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,17 @@
+var ZipPlugin = require('zip-webpack-plugin');
+
 // https://qiita.com/shin_moto/items/d75f1694bccf2bb4302d
 module.exports = {
-  mode: 'development', // 開発モードか本番モードかを設定出来る
+//  mode: 'development', // 開発モードか本番モードかを設定出来る
+  mode: 'production', // 開発モードか本番モードかを設定出来る
   entry: './src/build-entry.js', // バンドルの起点となるファイル
   output: {
     library: 'Pokedex',
     libraryTarget: 'umd',
     filename: 'Pokedex.js',
     globalObject: 'this',
-  }
+  },
+  plugins: [
+    new ZipPlugin({})
+  ]
 }
